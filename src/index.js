cb3f73b0e12608967ac1512805a919546d0171d8
Lukeofwar <matheuslucaskid@gmail.com>
2021-08-26 13:23:33 -0300

    criei um localhost com um hello world

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/projects', (request, response) => {
+         return response.send('Hello world');     
+}); 
+
+app.listen(3333);
