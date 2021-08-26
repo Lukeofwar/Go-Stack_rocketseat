704d482c8ccdfb5e50c174a59cd9e36043f2d12a
Lukeofwar <matheuslucaskid@gmail.com>
2021-08-26 18:47:29 -0300

    criei o localhost com o packge.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello gostac' });     
-}); 
+   return response.json({ message: 'Hello world' });        
+});
 
-app.listen(3333);
+app.listen(3333, () => {
+       console.log('🚀  Back-end started!');       
+});
