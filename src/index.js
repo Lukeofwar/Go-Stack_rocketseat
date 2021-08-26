810e0e3d05970742765f1ae05e99518ade41e4b3
Lukeofwar <matheuslucaskid@gmail.com>
2021-08-26 14:15:04 -0300

    coloquei uma extensão de reloaded automático

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-         return response.send('Hello world');     
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello gostac' });     
 }); 
 
 app.listen(3333);
