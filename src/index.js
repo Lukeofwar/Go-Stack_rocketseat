8235bdb79855d81f620ec73506f1b60351b67465
Lukeofwar <matheuslucaskid@gmail.com>
2021-08-27 11:05:25 -0300

    criei todas as functions http

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-   return response.json({ message: 'Hello world' });        
+app.get('/projects', (request, response) => {
+   return response.json([
+           'Projeto = 1',
+           'Projeto = 2',
+   ]);        
+});
+
+app.post('/projects', (request, response) => {
+   return response.json([
+           'Projeto = 1',
+           'Projeto = 2',
+           'project = 3',
+   ]);        
+});
+
+app.put('/projects/:id', (request, response) => {
+   return response.json([
+           'Projeto = 4',
+           'Projeto = 2',
+           'project = 3',
+   ]);        
+});
+
+app.delete('/projects/:id', (request, response) => {
+   return response.json([
+           'Projeto = 2',
+           'project = 3',
+   ]);        
 });
 
 app.listen(3333, () => {
