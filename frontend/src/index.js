5adb03fcc2b077b0fd7c1927fba922a3690bc3db
Lukeofwar <matheuslucaskid@gmail.com>
2021-09-21 11:49:19 -0300

    entendendo conceito de componentização

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import App from './App';
+ 
 // JSX: html demtro do javaScript ( javascript xml )
-render(<h1>Hello world</h1>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));        
