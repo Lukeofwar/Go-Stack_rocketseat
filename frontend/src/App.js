908c107230eb6538ead1788e22b8686e69eb73fa
Lukeofwar <matheuslucaskid@gmail.com>
2021-09-21 20:27:03 -0300

    criei uma lista ordenada usando html no javascript com a propriedade header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header'
 function App() {
      return (
           <>      
-       <Header />
-       <Header />
+       <Header title="Homepage" >
+            <ul>
+              <li>Homepage</li>   
+              <li>project devSetings</li>   
+            </ul>
+       </Header>
+       <Header title="project devtools" >
+            <ul>
+              <li>Homepage</li>   
+              <li>project devSetings</li>   
+              <li>Login</li>
+            </ul>
+       </Header>
        </>  
      );
 }
